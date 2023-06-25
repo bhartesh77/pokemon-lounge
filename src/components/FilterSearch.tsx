dd473bd5212140fe22af4672802d3f67d201af4f
bhartesh77 <bharteshkaushik@gmail.com>
2023-06-26 00:54:22 +0530

    Fix theme

--- src/components/FilterSearch.tsx
+++ src/components/FilterSearch.tsx
@@ -48,14 +48,18 @@ const FilterSearch = ({ heading, placeholder, suggestionsList }) => {
     setSelectedChips((prevChips) => prevChips.filter((c) => c !== chip));
     if (heading === "Abilities") {
       filtersContext.setSelectedFilters({
-        abilities: filtersContext.selectedFilters.abilities.filter(name => name !== chip),
+        abilities: filtersContext.selectedFilters.abilities.filter(
+          (name) => name !== chip
+        ),
         moves: filtersContext.selectedFilters.moves,
         names: filtersContext.selectedFilters.names,
       });
     } else {
       filtersContext.setSelectedFilters({
         abilities: filtersContext.selectedFilters.abilities,
-        moves: filtersContext.selectedFilters.moves.filter(name => name !== chip),
+        moves: filtersContext.selectedFilters.moves.filter(
+          (name) => name !== chip
+        ),
         names: filtersContext.selectedFilters.names,
       });
     }
@@ -63,48 +67,47 @@ const FilterSearch = ({ heading, placeholder, suggestionsList }) => {
 
   return (
     <>
-      <div className="mb-4 bg-yellow-200 rounded-lg p-4 shadow-md">
-      <h3 className="text-lg font-semibold mb-2 text-gray-800">{heading}</h3>
-      <div className="flex flex-wrap flex-col">
-        {selectedChips.map((chip, index) => (
-          <div
-            key={index}
-            className="bg-red-200 rounded-md px-2 py-1 m-1 flex items-center"
-          >
-            <span className="text-red-800">{chip}</span>
-            <button
-              className="ml-2 text-red-600 ml-auto"
-              onClick={() => handleChipClose(chip)}
-            >
-              X
-            </button>
-          </div>
-        ))}
-      </div>
-      <input
-        className="w-full border border-gray-300 rounded-md px-4 py-2 mt-4 mb-2"
-        type="text"
-        placeholder={placeholder}
-        value={searchText}
-        onChange={handleInputChange}
-      />
-      {searchText && suggestions.length > 0 && (
-        <ul className="bg-white border border-gray-300 rounded-md shadow-md">
-          {suggestions.map((suggestion, index) => (
-            <li
+      <div className="mb-4 rounded-lg p-4 shadow-lg bg-gray-900 text-white">
+        <h3 className="text-2xl font-semibold mb-2">{heading}</h3>
+        <div className="flex flex-wrap">
+          {selectedChips.map((chip, index) => (
+            <div
               key={index}
-              className="px-4 py-2 cursor-pointer hover:bg-gray-100"
-              onClick={() => handleSuggestionClick(suggestion)}
+              className="bg-purple-900 rounded-md px-2 py-1 m-1 flex items-center"
             >
-              {suggestion}
-            </li>
+              <span className="text-purple-100">{chip}</span>
+              <button
+                className="ml-2 text-purple-300"
+                onClick={() => handleChipClose(chip)}
+              >
+                X
+              </button>
+            </div>
           ))}
-        </ul>
-      )}
-    </div>
+        </div>
+        <input
+          className="w-full border border-gray-300 rounded-md px-4 py-2 mt-4 mb-2 bg-gray-800 text-white focus:outline-none"
+          type="text"
+          placeholder={placeholder}
+          value={searchText}
+          onChange={handleInputChange}
+        />
+        {searchText && suggestions.length > 0 && (
+          <ul className="bg-gray-800 border border-gray-700 rounded-md shadow-md">
+            {suggestions.map((suggestion, index) => (
+              <li
+                key={index}
+                className="px-4 py-2 cursor-pointer hover:bg-gray-700"
+                onClick={() => handleSuggestionClick(suggestion)}
+              >
+                {suggestion}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </>
   );
 };
-
 
 export default FilterSearch;
