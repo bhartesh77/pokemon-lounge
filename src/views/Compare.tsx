db442c7475d57ee8de0f7e4d1a9287f65dc59ec5
bhartesh77 <bharteshkaushik@gmail.com>
2023-07-03 00:09:45 +0530

    Fix web view

--- src/views/Compare.tsx
+++ src/views/Compare.tsx
@@ -3,6 +3,7 @@ import CompareCard from "../components/CompareCard";
 import Poke from "../assets/Poke.png";
 import "../App.css";
 import Header from "../components/Header";
+import isMobile from "../utils/isMobile";
 
 const Compare = () => {
   const [pokemon1, setPokemon1] = useState("");
@@ -36,14 +37,64 @@ const Compare = () => {
     searchPokemon(pokemon2.toLowerCase(), setPokemonData2);
   };
 
+  if (isMobile())
+    return (
+      <div className="h-screen w-screen">
+        <Header />
+        <div
+          className={`transition-transform duration-300 ${
+            pokemonData1 && pokemonData2
+              ? "flex mb-4 w-screen px-16 justify-between pt-4"
+              : "flex flex-col w-[screen] left-8 absolute justify-center h-screen items-center"
+          }`}
+        >
+          <input
+            type="text"
+            value={pokemon1}
+            onChange={handlePokemon1Change}
+            placeholder="Enter Pokémon 1"
+            className="bg-slate-200 px-4 py-2 w-44 mb-4 border border-gray-300 rounded-full focus:outline-none focus:border-blue-500 shadow-neon shadow-2xl"
+          />
+          <input
+            type="text"
+            value={pokemon2}
+            onChange={handlePokemon2Change}
+            placeholder="Enter Pokémon 2"
+            className="bg-slate-200 px-4 py-2 w-44 border border-gray-300 rounded-full focus:outline-none focus:border-blue-500 shadow-neon"
+          />
+        </div>
+
+        <div className="flex w-screen justify-between px-16">
+          {pokemonData1 && pokemonData2 && (
+            <CompareCard data1={pokemonData1} data2={pokemonData2} />
+          )}
+          {pokemonData2 && pokemonData1 && (
+            <CompareCard data1={pokemonData2} data2={pokemonData1} />
+          )}
+        </div>
+
+        <div className="w-[100%] flex justify-center animate-pulse cursor-pointer">
+          <div
+            onClick={handleSearch}
+            className={`h-24 w-24 right-8 absolute flex items-center justify-center ${
+              pokemonData1 && pokemonData2
+                ? "bottom-8"
+                : "top-1/2 transform -translate-y-1/2"
+            } text-white font-semibold rounded-full hover:scale-110 transition-transform duration-300 shadow-2xl focus:outline-none`}
+          >
+            <img src={Poke} className="absolute h-24" alt="Pokeball" />
+          </div>
+        </div>
+      </div>
+    );
+
   return (
     <div className="h-screen w-screen">
-      <Header />
       <div
-        className={`transition-transform duration-300 ${
+        className={` transition-transform duration-300 ${
           pokemonData1 && pokemonData2
             ? "flex mb-4 w-screen px-16 justify-between pt-4"
-            : "flex flex-col w-[screen] left-8 absolute justify-center h-screen items-center"
+            : "flex w-[screen] px16 justify-center h-screen items-center"
         }`}
       >
         <input
@@ -51,14 +102,14 @@ const Compare = () => {
           value={pokemon1}
           onChange={handlePokemon1Change}
           placeholder="Enter Pokémon 1"
-          className="bg-slate-200 px-4 py-2 w-44 mb-4 border border-gray-300 rounded-full focus:outline-none focus:border-blue-500 shadow-neon shadow-2xl"
+          className="bg-slate-200 px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:border-blue-500 mr-32 shadow-neon shadow-2xl"
         />
         <input
           type="text"
           value={pokemon2}
           onChange={handlePokemon2Change}
           placeholder="Enter Pokémon 2"
-          className="bg-slate-200 px-4 py-2 w-44 border border-gray-300 rounded-full focus:outline-none focus:border-blue-500 shadow-neon"
+          className="bg-slate-200 px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:border-blue-500 shadow-neon"
         />
       </div>
 
@@ -74,7 +125,7 @@ const Compare = () => {
       <div className="w-[100%] flex justify-center animate-pulse cursor-pointer">
         <div
           onClick={handleSearch}
-          className={`h-24 w-24 right-8 absolute flex items-center justify-center ${
+          className={`h-24 w-24 absolute flex items-center justify-center ${
             pokemonData1 && pokemonData2
               ? "bottom-8"
               : "top-1/2 transform -translate-y-1/2"
